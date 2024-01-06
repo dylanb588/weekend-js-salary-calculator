ca4c48577ec11b0423407a8c56a9422febf2b973
Dylan Brown <dylanbrown588@gmail.com>
2024-01-06 17:17:21 -0600

    create a function to add up the saleries and add it to the dom as a monthly budget

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ function addEmployee(event) {
     let lastNameValue = document.getElementById("last-name").value;
     let IDValue = document.getElementById("id").value;
     let jobTitleValue = document.getElementById("job-title").value;
-    let salaryValue = document.getElementById("salary").value;
+    let salaryValue = parseFloat(document.getElementById("salary").value);
     // Using the input values it creates an object and adds it to the global array
     const employee = {
-        firstName: `${firstNameValue}`,
-        lastName: `${lastNameValue}`,
-        idNumber: `${IDValue}`,
-        jobTitle: `${jobTitleValue}`,
-        annualSalary: `${salaryValue}`
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        idNumber: IDValue,
+        jobTitle: jobTitleValue,
+        annualSalary: salaryValue.toLocaleString()
     };
         
     employeeList.push(employee);
@@ -33,6 +33,7 @@ function addEmployee(event) {
     document.getElementById("salary").value = "";
     // Runs a function to update the table
     updateTable(employeeList);
+    calculateBudget(employeeList);
 }
 
 
@@ -42,7 +43,6 @@ function updateTable(data) {
     tableBody.innerHTML = "";
     
     for (let i = 0; i < data.length; i++) {
-        let formattedSalary = parseFloat(data[i].annualSalary).toLocaleString();
 
         let row = `
         <tr>
@@ -50,7 +50,7 @@ function updateTable(data) {
             <td>${data[i].lastName}</td>
             <td>${data[i].idNumber}</td>
             <td>${data[i].jobTitle}</td>
-            <td>$${formattedSalary}</td>
+            <td>$${data[i].annualSalary}</td>
             <td><button>Delete</button></td>
         </tr>`
 
@@ -58,3 +58,33 @@ function updateTable(data) {
     }
 }
 
+function calculateBudget(employeeList) {
+    if (employeeList.length === 0){
+        return;
+    }
+    
+    let totalAnnualSalary = 0;
+
+    for (let i = 0; i < employeeList.length; i++) {
+        const noCommas = parseFloat(employeeList[i].annualSalary.replace(/,/g,''));
+        totalAnnualSalary += noCommas
+    }
+
+    let monthlyBudget = totalAnnualSalary / 12;
+
+    const budgetList = document.getElementById("monthly-budget");
+
+    budgetList.innerHTML = "";
+
+    const formatBudget = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    }).format(monthlyBudget);
+
+    if (monthlyBudget > 20000) {
+        budgetList.innerHTML += `<p style="background-color: red;">Monthly Budget: ${formatBudget}</p>`;
+    } else {
+        budgetList.innerHTML += `<p>Monthly Budget: ${formatBudget}</p>`;
+    }
+}
